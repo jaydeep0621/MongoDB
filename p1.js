79f5d47602c5b4a2c866b182cb13688befb23515
unknown <jaydeep0216@gmail.com>
2020-07-08 17:58:15 +0530

    test

--- p1.js
+++ p1.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
+
 mongoose.Promise = global.Promise; //Necessary condition using ES6
 
+
+//Creating Connection with DB own on local otherwise URL
 mongoose.connect("mongodb://localhost/student",{useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex:true});
 
-
+//Handling the error
 mongoose.connection
     .once('open',() => console.log("Connected"))
     .on('error',(error)=> {
